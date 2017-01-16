d7c3109c19b9aebe45c0c6f5aeb6e8f8a5b688e4
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2017-01-16 16:00:01 +0100

    add styles and content for news page

--- code/themes/wilmap/lib/main.js
+++ code/themes/wilmap/lib/main.js
@@ -7,7 +7,9 @@ function getAbsolutePath() {
 (function ($) {
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-      //console.log(settings.path.currentPath);
+      console.log(settings);
+      // console.log(settings.path.currentPath);
+
       var path = getAbsolutePath();
       if ($(context).find('.topics-page').length !== 0) {
         var gallerytopics = document.querySelector(".gallery-topics");
@@ -19,7 +21,6 @@ function getAbsolutePath() {
             "Content-Type": "application/hal+json"
           },
           success: function(data, status, xhr) {
-            console.log('hi');
             for(var i = 0; i<data.length; i++){
               var contentbox = '<a href="#" class="info-topics"><div><h3>' + data[i].field_name + '</h3>' +
               '<hr><p class="paragraph">' + data[i].field_d + '</p></div></a>';
