522c77460229b7054a9496f0db681ad2734d6a3d
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2017-01-25 12:21:10 +0100

    fix errors

--- gulpfile.js
+++ gulpfile.js
@@ -10,43 +10,43 @@ var pngquant = require('imagemin-pngquant');
 
 
 gulp.task('imagemin', function () {
-    return gulp.src('./code/themes/wilmap/images/*')
+    return gulp.src('./themes/wilmap/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./code/themes/wilmap/images'));
+        .pipe(gulp.dest('./themes/wilmap/images'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('./code/themes/wilmap/sass/**/*.scss')
+  gulp.src('./themes/wilmap/sass/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./code/themes/wilmap/css'));
+    .pipe(gulp.dest('./themes/wilmap/css'));
 });
 
 
 gulp.task('uglify', function() {
-  gulp.src('./code/themes/wilmap/lib/*.js')
+  gulp.src('./themes/wilmap/lib/*.js')
     .pipe(uglify('main.js', 'select2.full.min.js'))
     .pipe(gulp.dest('./code/themes/wilmap/js'));
 });
 
 gulp.task('uglify', function () {
-  gulp.src('./code/themes/wilmap/lib/*.js')
+  gulp.src('./themes/wilmap/lib/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./code/themes/wilmap/js'));
 });
 
 gulp.task('watch', function(){
   livereload.listen();
-  gulp.watch('./code/themes/wilmap/sass/**/*.scss', ['sass']);
-  gulp.watch('./code/themes/wilmap/lib/*.js', ['uglify']);
-  gulp.watch(['./code/themes/wilmap/css/style.css', './code/themes/wilmap/**/*.twig', './code/themes/wilmap/js/*.js'], function (files) {
+  gulp.watch('./themes/wilmap/sass/**/*.scss', ['sass']);
+  gulp.watch('./themes/wilmap/lib/*.js', ['uglify']);
+  gulp.watch(['./themes/wilmap/css/style.css', './themes/wilmap/**/*.twig', './themes/wilmap/js/*.js'], function (files) {
       livereload.changed(files)
     });
 });
