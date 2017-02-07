082d14dd64ce6fb27f2b6aa2f895e84e4df68c10
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2017-02-07 17:47:49 +0100

    improve .js files wit new modules and change admin credentials

--- themes/wilmap/lib/main.js
+++ themes/wilmap/lib/main.js
@@ -127,14 +127,14 @@ function changeMenuOption(option) {
         });
 
         $.ajax({
-          url: `${path}api/newsJSON`,
+          url: `${path}api/newsJSON?items_per_page=3&page=1`,
           method: 'GET',
           headers: {
             Accept: 'application/json',
             'Content-Type': 'application/hal+json'
           },
           success: function showNews(data, status, xhr) {
-            for (let i = 0; i < 3; i += 1) {
+            for (let i = 0; i < data.length; i += 1) {
               const contentbox = `<div data-category="${data[i].field_category}" class="info-news">
               <h2>${data[i].title}</h2>
               <span class="date">${data[i].field_date_published}</span>
