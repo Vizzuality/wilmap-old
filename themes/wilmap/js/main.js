2c0329f3344cdd58364c9878d8cdece5482aca41
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2017-02-09 17:18:24 +0100

    map page, basics styles and functions

--- themes/wilmap/js/main.js
+++ themes/wilmap/js/main.js
@@ -18,7 +18,7 @@ function changeMenuOption(option) {
       var host = window.location.host;
 
       // *******************************************************
-      // FUNCTIONS FOR GALLERY ALL PAGES
+      // FUNCTIONS FOR ALL PAGES
       // *******************************************************
 
       $('.search-box').click(function () {
@@ -53,6 +53,22 @@ function changeMenuOption(option) {
           }
         });
       });
+
+      // *******************************************************
+      // FUNCTIONS FOR MAP PAGE
+      // *******************************************************
+      if (settings.path.currentPath === 'map') {
+        $.getJSON(path + 'api/continentsJSON', function (data) {
+          for (var i = 0; i < data.length; i += 1) {
+            $('.list-country-search-map').append('<li>' + data[i].title + '</li>');
+          }
+        });
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaGVjdG9ydWNoIiwiYSI6ImNpeXk3NzgzMjAwMDYzM3BuNXdiN3NiMDAifQ.v801v1GQOc5LhKNe5cAplQ';
+        var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/hectoruch/ciyy8zgf900242sln9d7630km'
+        });
+      }
 
       // *******************************************************
       // FUNCTIONS FOR GALLERY EXPLORE PAGE
--- themes/wilmap/lib/main.js
+++ themes/wilmap/lib/main.js
@@ -17,7 +17,7 @@ function changeMenuOption(option) {
       const host = window.location.host;
 
       // *******************************************************
-      // FUNCTIONS FOR GALLERY ALL PAGES
+      // FUNCTIONS FOR ALL PAGES
       // *******************************************************
 
       $('.search-box').click(function(){
@@ -52,6 +52,22 @@ function changeMenuOption(option) {
           }
         });
       });
+
+      // *******************************************************
+      // FUNCTIONS FOR MAP PAGE
+      // *******************************************************
+      if (settings.path.currentPath === 'map') {
+        $.getJSON(`${path}api/continentsJSON`, function (data) {
+          for (let i = 0; i < data.length; i += 1) {
+            $('.list-country-search-map').append(`<li>${data[i].title}</li>`);
+          }
+        });
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaGVjdG9ydWNoIiwiYSI6ImNpeXk3NzgzMjAwMDYzM3BuNXdiN3NiMDAifQ.v801v1GQOc5LhKNe5cAplQ';
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/hectoruch/ciyy8zgf900242sln9d7630km'
+        });
+      }
 
       // *******************************************************
       // FUNCTIONS FOR GALLERY EXPLORE PAGE
