14feb8ed545469b50e1b682d6f9e18ce2a010186
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2017-03-01 18:56:00 +0100

    colour map with borders (WIP)

--- themes/wilmap/dist/pages/map.js
+++ themes/wilmap/dist/pages/map.js
@@ -4,8 +4,7 @@
   mapboxgl.accessToken = 'pk.eyJ1IjoiaGVjdG9ydWNoIiwiYSI6ImNpeXk3NzgzMjAwMDYzM3BuNXdiN3NiMDAifQ.v801v1GQOc5LhKNe5cAplQ';
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/hectoruch/ciyy8zgf900242sln9d7630km'
+    style: 'mapbox://styles/hectoruch/cizr9hdsh00o22rpdwsk30jfg'
   });
-
   var mapSidebar = new App.MapSidebar('.list-country-search-map');
 })();
--- themes/wilmap/lib/pages/map.js
+++ themes/wilmap/lib/pages/map.js
@@ -2,8 +2,7 @@
   mapboxgl.accessToken = 'pk.eyJ1IjoiaGVjdG9ydWNoIiwiYSI6ImNpeXk3NzgzMjAwMDYzM3BuNXdiN3NiMDAifQ.v801v1GQOc5LhKNe5cAplQ';
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/hectoruch/ciyy8zgf900242sln9d7630km'
+    style: 'mapbox://styles/hectoruch/cizr9hdsh00o22rpdwsk30jfg'
   });
-
   const mapSidebar = new App.MapSidebar('.list-country-search-map');
 })();
