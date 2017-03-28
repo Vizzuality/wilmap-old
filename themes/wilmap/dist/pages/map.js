5396872a417901665dcda48417a8e2dceacac7d7
erik <erik.escoffier@gmail.com>
2017-03-28 17:24:17 +0200

    continent/country selection

--- themes/wilmap/dist/pages/map.js
+++ themes/wilmap/dist/pages/map.js
@@ -1,13 +1,47 @@
 'use strict';
 
 (function () {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiaGVjdG9ydWNoIiwiYSI6ImNpeXk3NzgzMjAwMDYzM3BuNXdiN3NiMDAifQ.v801v1GQOc5LhKNe5cAplQ';
+  mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbWFwMiIsImEiOiJjajB0a2tvamowMmxmMndvN3IzdWc0a2RkIn0.CD_s7nTMkFLz5ZA4NpOz0A';
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/hectoruch/cizr9hdsh00o22rpdwsk30jfg',
+    style: 'mapbox://styles/wilmap2/cj0tkrrvy00hw2rqpc5selayi',
     center: [0, 0],
     zoom: 2
   });
 
-  var sidebar = new App.Component.MapAccordion('.list-country-search-map');
+  map.on('load', function () {
+    map.setFilter('countries', ['==', 'name', '']);
+    map.setFilter('continents', ['==', 'CONTINENT', '']);
+    // map.setPaintProperty('bg_countries', 'fill-color', '#faafee');
+  });
+
+  var continentSelected = void 0;
+
+  map.on('click', function (e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: ['countries_bg', 'continents_bg'] });
+
+    var country = features.find(function (feature) {
+      return feature.layer.id === 'countries_bg';
+    });
+    var continent = features.find(function (feature) {
+      return feature.layer.id === 'continents_bg';
+    });
+    if (country === undefined || continent === undefined) {
+      console.warn('nothing here.');
+      return;
+    }
+
+    if (continentSelected !== continent.properties.CONTINENT) {
+      // select continent
+      continentSelected = country.properties.continent;
+      map.setFilter('continents', ['==', 'CONTINENT', continentSelected]);
+
+      // reset country
+      map.setFilter('countries', ['==', 'name', '']);
+    } else {
+      // select country
+      map.setFilter('countries', ['==', 'name', country.properties.name]);
+    }
+  });
+  // const sidebar = new App.Component.MapAccordion('.list-country-search-map');
 })();
--- themes/wilmap/lib/pages/map.js
+++ themes/wilmap/lib/pages/map.js
@@ -1,11 +1,41 @@
 (() => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiaGVjdG9ydWNoIiwiYSI6ImNpeXk3NzgzMjAwMDYzM3BuNXdiN3NiMDAifQ.v801v1GQOc5LhKNe5cAplQ';
+  mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbWFwMiIsImEiOiJjajB0a2tvamowMmxmMndvN3IzdWc0a2RkIn0.CD_s7nTMkFLz5ZA4NpOz0A';
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/hectoruch/cizr9hdsh00o22rpdwsk30jfg',
+    style: 'mapbox://styles/wilmap2/cj0tkrrvy00hw2rqpc5selayi',
     center: [0, 0],
     zoom: 2
-});
+  });
 
-  const sidebar = new App.Component.MapAccordion('.list-country-search-map');
+  map.on('load', () => {
+    map.setFilter('countries', ['==', 'name', '']);
+    map.setFilter('continents', ['==', 'CONTINENT', '']);
+    // map.setPaintProperty('bg_countries', 'fill-color', '#faafee');
+  });
+
+  let continentSelected;
+
+  map.on('click', (e) => {
+    const features = map.queryRenderedFeatures(e.point, { layers: ['countries_bg', 'continents_bg'] });
+
+    const country = features.find(feature => feature.layer.id === 'countries_bg');
+    const continent = features.find(feature => feature.layer.id === 'continents_bg');
+    if (country === undefined || continent === undefined) {
+      console.warn('nothing here.');
+      return;
+    }
+
+    if (continentSelected !== continent.properties.CONTINENT) {
+      // select continent
+      continentSelected = country.properties.continent;
+      map.setFilter('continents', ['==', 'CONTINENT', continentSelected]);
+
+      // reset country
+      map.setFilter('countries', ['==', 'name', '']);
+    } else {
+      // select country
+      map.setFilter('countries', ['==', 'name', country.properties.name]);
+    }
+  });
+  // const sidebar = new App.Component.MapAccordion('.list-country-search-map');
 })();
