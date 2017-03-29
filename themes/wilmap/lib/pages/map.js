296668a94f01c099be82674fba5db9f4132e17f6
erik <erik.escoffier@gmail.com>
2017-03-29 19:14:58 +0200

    map

--- themes/wilmap/lib/pages/map.js
+++ themes/wilmap/lib/pages/map.js
@@ -1,51 +1,132 @@
 (() => {
-  mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbWFwMiIsImEiOiJjajB0a2tvamowMmxmMndvN3IzdWc0a2RkIn0.CD_s7nTMkFLz5ZA4NpOz0A';
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/wilmap2/cj0v5s204010w2rt8oz3r89vj',
-    center: [0, 0],
-    zoom: 2
-  });
-
   const popupTemplate = _.template($('#map-popup-template').html());
+  let popup = new mapboxgl.Popup();
 
-  map.on('load', () => {
+  let currentContinent;
+  let currentCountry;
+  let map;
+
+  const resetCountry = () => {
+    currentCountry = '';
     map.setFilter('countries', ['==', 'name', '']);
-    map.setFilter('continents', ['==', 'CONTINENT', '']);
-  });
+    popup.remove();
+  };
 
-  let continentSelected;
+  const selectContinent = (continentId) => {
+    currentContinent = (continentId === currentContinent) ? '' : continentId;
+    map.setFilter('continents', ['==', 'CONTINENT', currentContinent]);
 
-  map.on('click', (e) => {
-    const features = map.queryRenderedFeatures(e.point, { layers: ['countries_bg', 'continents_bg'] });
+    $('.js-accordion').toggleClass('-collapsed', currentContinent === '');
 
-    const country = features.find(feature => feature.layer.id === 'countries_bg');
-    const continent = features.find(feature => feature.layer.id === 'continents_bg');
-    if (country === undefined || continent === undefined) {
-      console.warn('nothing here.');
+    resetCountry();
+
+    $('.js-accordion .js-continent-container').removeClass('-open');
+    $(`.js-accordion .js-continent-container[data-id='${currentContinent}']`).addClass('-open');
+  };
+
+  const selectCountry = (countryId, point) => {
+    if (currentCountry === countryId) {
+      resetCountry();
+      return;
+    }
+    currentCountry = countryId;
+    const countryFilter = ['==', 'name', currentCountry];
+    map.setFilter('countries', countryFilter);
+
+    const countries = map.queryRenderedFeatures({
+      layers: ['countries_bg'],
+      filter: countryFilter
+    });
+
+    if (!countries.length) {
       return;
     }
 
-    if (continentSelected !== continent.properties.CONTINENT) {
-      // select continent
-      continentSelected = country.properties.continent;
-      map.setFilter('continents', ['==', 'CONTINENT', continentSelected]);
+    const center = (point === undefined) ?
+      turf.centroid(countries[0]).geometry.coordinates :
+      map.unproject(point);
 
-      // reset country
+    popup.remove();
+
+    popup = new mapboxgl.Popup()
+    .setLngLat(center)
+    .setHTML(popupTemplate({
+      name: countryId,
+      link: 'https://www.yobyot.com/wp-content/uploads/2011/04/hatedrupal.jpg'
+    }))
+    .addTo(map);
+  };
+
+  const init = () => {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbWFwMiIsImEiOiJjajB0a2tvamowMmxmMndvN3IzdWc0a2RkIn0.CD_s7nTMkFLz5ZA4NpOz0A';
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/wilmap2/cj0v5s204010w2rt8oz3r89vj',
+      center: [0, 0],
+      zoom: 2
+    });
+
+    map.on('load', () => {
       map.setFilter('countries', ['==', 'name', '']);
-    } else {
-      // select country
-      map.setFilter('countries', ['==', 'name', country.properties.name]);
+      map.setFilter('continents', ['==', 'CONTINENT', '']);
+    });
 
-      const popup = new mapboxgl.Popup()
-        .setLngLat(map.unproject(e.point))
-        .setHTML(popupTemplate({
-          name: country.properties.name,
-          link: 'https://www.yobyot.com/wp-content/uploads/2011/04/hatedrupal.jpg'
-        }))
-        .addTo(map);
-    }
-  });
+    map.on('click', (e) => {
+      const features = map.queryRenderedFeatures(e.point, { layers: ['countries_bg', 'continents_bg'] });
 
-  // const sidebar = new App.Component.MapAccordion('.list-country-search-map');
+      const country = features.find(feature => feature.layer.id === 'countries_bg');
+      const continent = features.find(feature => feature.layer.id === 'continents_bg');
+      if (country === undefined || continent === undefined) {
+        console.warn('nothing here.');
+        return;
+      }
+
+      if (currentContinent !== continent.properties.CONTINENT) {
+        selectContinent(country.properties.continent);
+      } else {
+        selectCountry(country.properties.name, e.point);
+      }
+    });
+
+    const accordionTemplate = _.template($('#map-accordion-template').html());
+
+    const fetchPromises = ['api/continentsJSON', 'api/regionsJSON', 'api/countriesJSON'].map(url => fetch(url));
+    Promise.all(fetchPromises)
+    .then(responses => Promise.all(responses.map(res => res.text())))
+    .then(texts => Promise.all(texts.map(text => JSON.parse(text))))
+    .then((jsons) => {
+      const continents = jsons[0].slice(3);
+      const regions = jsons[1];
+      const countries = jsons[2];
+
+      regions.forEach((region) => {
+        const regionContinent = continents.find(continent => continent.nid === region.field_continent);
+        if (regionContinent) {
+          if (regionContinent.regions === undefined) {
+            regionContinent.regions = [];
+          }
+          regionContinent.regions.push(region);
+        }
+      });
+      countries.forEach((country) => {
+        const countryContinent = continents.find(continent => continent.nid === country.field_continent_country);
+        if (countryContinent) {
+          if (countryContinent.countries === undefined) {
+            countryContinent.countries = [];
+          }
+          countryContinent.countries.push(country);
+        }
+      });
+
+      console.warn(continents);
+      $('.js-accordion').html(accordionTemplate({ continents }));
+
+      $('.js-accordion .js-continent').on('click', (e) => {
+        selectContinent($(e.currentTarget).parent('.js-continent-container').data().id);
+      });
+      $('.js-accordion .js-country').on('click', (e) => { selectCountry($(e.currentTarget).data().id); });
+    });
+  };
+
+  init();
 })();
