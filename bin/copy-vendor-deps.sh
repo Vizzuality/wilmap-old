0ed2855736c90fc0a62b475a57bddb6bbeaa3ee1
erik <erik.escoffier@gmail.com>
2017-03-29 18:12:55 +0200

    lodash

--- bin/copy-vendor-deps.sh
+++ bin/copy-vendor-deps.sh
@@ -5,3 +5,4 @@ cp ./node_modules/select2/dist/js/select2.full.min.js ./themes/wilmap/vendor/js/
 cp ./node_modules/select2/dist/css/select2.min.css ./themes/wilmap/vendor/css/select2.min.css
 cp ./node_modules/mapbox-gl/dist/mapbox-gl.js ./themes/wilmap/vendor/js/mapbox-gl.js
 cp ./node_modules/mapbox-gl/dist/mapbox-gl.css ./themes/wilmap/vendor/css/mapbox-gl.css
+cp ./node_modules/lodash/lodash.min.js ./themes/wilmap/vendor/js/lodash.min.js
