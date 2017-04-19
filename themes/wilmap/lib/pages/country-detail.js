bcbb017590668a7b714dcccebc4f30f3d2066750
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2017-04-19 08:32:54 +0200

    update database and add new styles

--- themes/wilmap/lib/pages/country-detail.js
+++ themes/wilmap/lib/pages/country-detail.js
@@ -2,13 +2,17 @@
   let map;
   const nodeid = settings.path.currentPath.split('/').pop();
 
-  const tabs = new App.Component.Tabs('.list-categories', {
+  const getContentCountry = () => { // Show entries and news on country detail page
+    let urlJSON = '';
+  };
+
+  const tabs = new App.Component.Tabs('.list-categories', { // Tabs
     fetch: true,
     endpoint: '/api/categoriesJSON',
-    // callback: getPager
+    callback: getContentCountry
   });
 
-  const initMap = () => {
+  const initMap = () => { // Init map on header
     mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbWFwMiIsImEiOiJjajB0a2tvamowMmxmMndvN3IzdWc0a2RkIn0.CD_s7nTMkFLz5ZA4NpOz0A';
     map = new mapboxgl.Map({
       container: 'mapCover',
@@ -23,14 +27,5 @@
     });
   };
 
-  const initTabs = () => {};
-  const showCountryEntries = () => {};
-  const showCountryNews = () => {
-    $('.content-country-news').html('hello');
-  };
-
   initMap();
-  showCountryEntries();
-  showCountryNews();
-  initTabs()
 })(jQuery, drupalSettings);
